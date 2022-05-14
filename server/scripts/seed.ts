2d397fba98d1d88fe3f16754e5acc0e98156b42c
Tom Tang <jjjj1997@live.hk>
2022-05-14 15:56:36 +0800

    fix: @nestjs/cli vulnerabilities, seed example user is admin

--- server/scripts/seed.ts
+++ server/scripts/seed.ts
@@ -28,7 +28,7 @@ async function seed(bcryptSalt: Salt) {
   const data = {
     username: "admin",
     password: await hash("admin", bcryptSalt),
-    roles: ["user"],
+    roles: ["admin"],
     email: "example@example.com",
   };
   await client.user.upsert({
